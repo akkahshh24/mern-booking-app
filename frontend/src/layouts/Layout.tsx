6968aad6054e0a513ff9dbaddc174dc94c8278b0
Akkahshh <akkahshh24@gmail.com>
2024-01-01 12:44:18 +0530

    add header and footer

---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,23 @@
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Footer from '../components/Footer';
+import React from 'react';
+
+interface Props {
+    children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header></Header>
+            <Hero></Hero>
+            <div className='container mx-auto py-10 flex-1'>
+                {children}
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Layout;
